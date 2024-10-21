37d0ca7c08007a6f8a208ea3924fc6bfc6c42748
Kostaintyn <kostiantyn208148@student-schoolsofwestfield.org>
2024-10-21 16:12:16 +0000

    1

--- katas-1.js
+++ katas-1.js
@@ -59,8 +59,14 @@
 
 /* Var Alert Prompt Conditionals */
 // make a variable for pet type, prompt user for it 
+var petType = prompt("What's type your pet?");
 // make a variable for pet name, prompt user for it
+var petName = prompt("What is your pet's name?");
 // alert "You have a pet type named pet name 
+alert("You have a " + petType + " named " + petName + ".");
 // if pet is a dog, say "I like dogs, too!"
+if ( petType == "dog") alert("I like dogs, too!");
 // if it is a cat, say "I'm allergic to cats"
+else if ( petType == "cat" ) alert("I'm allergic to cats");
 // If it is not a dog or a cat, say "what an interesting pet!"
+else alert("what an interesting pet!");
